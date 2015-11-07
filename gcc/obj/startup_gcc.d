de1708bf0c2bc2bdbf43157f3af2992293f6bfa0
Andthen <andzenn@gmail.com>
2015-11-07 22:23:20 +0800

    remove gcc/obj/*.d and gcc/obj/*.bin exe

--- gcc/obj/startup_gcc.d
+++
@@ -1,6 +0,0 @@
-obj/startup_gcc.o: ../../common/startup_gcc.c \
- c:\cygwin\lib\gcc\arm-none-eabi\4.8.4\include\stdint.h \
- c:\cygwin\arm-none-eabi\include\stdint.h \
- c:\cygwin\arm-none-eabi\include\machine\_default_types.h \
- c:\cygwin\arm-none-eabi\include\sys\features.h ../../../inc/hw_nvic.h \
- ../../../inc/hw_types.h
